e221eed1f453821f32593ba4a94697b30dd831e4
Amal Joy <amaljoy258@gmail.com>
2020-11-01 21:57:38 +0530

    Assignment 4 solution added

--- 4/script/function.js
+++ 4/script/function.js
@@ -1,5 +1,5 @@
 (function (window) {
-var names = ["Jerom", "Sara", "Ann", "Jomon", "Paulson", "Kate", "Suman", "Jacky", "Lia", "Edwin"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
   if (firstLetter == 'j') {
